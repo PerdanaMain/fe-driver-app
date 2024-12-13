db5bffb9d8a72e0b65937c814bcdb274cd18157e
Perdana Main <firman.fp123@gmail.com>
2024-12-13 15:11:09 +0700

    feat: add lib

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 // app/api/auth/[...nextauth]/route.js
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { apiUrl } from "../../../lib/config";
 
 const handler = NextAuth({
   providers: [
@@ -12,7 +13,7 @@ const handler = NextAuth({
       },
       async authorize(credentials) {
         try {
-          const res = await fetch("http://localhost:5000//api/v1/login", {
+          const res = await fetch(`${apiUrl}/api/v1/login`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
